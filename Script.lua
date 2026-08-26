59bb833dd2284b5e745fe1f0d966997e1a2679e2
thienduc190311-cmyk <thienduc190311@gmail.com>
2026-08-26 14:38:20 +0700

    Add basic GUI with speed boost feature

---
+++ Script.lua
@@ -0,0 +1,19 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "My First Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+-- Tạo 1 Tab chính
+local MainTab = Window:MakeTab({
+	Name = "Tính năng",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+-- Tạo một nút bấm (Button)
+MainTab:AddButton({
+	Name = "Tăng tốc độ chạy",
+	Callback = function()
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
+  	end    
+})
+
+OrionLib:Init()
